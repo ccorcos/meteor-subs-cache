650682cdc9c3e22588ec5beb4bb1d2ee12cf454f
Chet Corcos <ccorcos@gmail.com>
2015-03-11 14:56:39 -0700

    version and history

---
+++ HISTORY.md
@@ -0,0 +1,3 @@
+# 0.0.2
+
+Added onReady callbacks. Also forgot the reactive-var dependency
--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ccorcos:subs-cache',
   summary: 'A package for caching Meteor subscriptions.',
-  version: '0.0.1',
+  version: '0.0.2',
   git: 'https://github.com/ccorcos/meteor-subs-cache'
 });
 
