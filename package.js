7757056e3df4b74876d983e470b361c55626b9b0
Esteban Paredes <par_boletin@sistemas5sol.com>
2017-10-26 20:27:41 -0500

    Minor correction to allow publishing at Atmosphere

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ccorcos:subs-cache',
   summary: 'A package for caching Meteor subscriptions.',
-  version: '0.9.2',
+  version: '0.9.3',
   git: 'https://github.com/ccorcos/meteor-subs-cache'
 });
 
@@ -9,9 +9,8 @@ Package.onUse(function(api) {
   api.versionsFrom('METEOR@1');
 
   api.use([
-    'ecmascript',
+    'ecmascript@0.8.3',
     'underscore',
-    'ecmascript',
     'ejson',
     'tracker',
     'reactive-var'
