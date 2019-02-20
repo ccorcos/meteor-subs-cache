4433dc5a2657c4787c36b2b635f14e50f248c245
paranoico <par_boletin@sistemas5sol.com>
2019-02-20 17:28:06 -0600

    Versión update after #67

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "ccorcos:subs-cache",
   summary: "A package for caching Meteor subscriptions.",
-  version: "0.9.11",
+  version: "0.9.12",
   git: "https://github.com/ccorcos/meteor-subs-cache"
 });
 
